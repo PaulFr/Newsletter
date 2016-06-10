a5e249af84e40959be0195d606f70797b8b4af28
Paul Frinchaboy <pfrinchaboy@gmail.com>
2016-06-10 14:44:29 +0200

    Verifies if the user is logged in and if he's not, redirects to login page

--- app/core/AppController.php
+++ app/core/AppController.php
@@ -1,5 +1,19 @@
 <?php
 
 class AppController extends Controller_SW{
-	
+	public $requiredPlugins = array('Session');
+	public function __construct($request, $response){
+		parent::__construct($request, $response);
+		if($request->controller != 'tracks' && $request->controller != 'users' && !isset($this->Session->get('User')->firstname)){
+			/*if(!(isset($this->Session->get('User')->rights) && $this->Session->get('User')->rights & Rights::get('ACCESS_ADMIN'))){
+				Core::throwError(404);
+			}*/
+			$this->response->redirect('');
+		}
+	}
 }
+
+
+
+
+            
